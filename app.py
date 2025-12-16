1ae4c0c01e23af3a00fe57604b8bd0d7d14dcf22
jyotirajkumari38-cpu <jyotirajkumari38@gmail.com>
2025-12-16 20:08:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-import plotly.express as px
+import plotly.express as px   
 import yfinance as yf
 import requests
 import time
